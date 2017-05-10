ff0546ca03ad876c197b6a5e7775f963ea2a7a71
David Gold <david.gold.a@gmail.com>
2017-05-09 23:58:19 -0700

    Fix cluster direction

--- src/sims/sems1/sems1.R
+++ src/sims/sems1/sems1.R
@@ -1,15 +1,9 @@
 # setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
+library(magrittr)
 source("sems1_ker.R")
 sa_id <- Sys.getenv('SLURM_ARRAY_TASK_ID') %>% as.numeric
 
 set.seed(sa_id)
 sim(sa_id) %>%
   write.csv(file = paste("res/res", sa_id, ".csv", sep = ""))
-
-
-
-
-  
-
-
