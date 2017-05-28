c38e9f5e44e6d31a4c7716635bf1935d8ee9cde7
David Gold <david.gold.a@gmail.com>
2017-05-28 13:16:35 -0700

    Get around solve issues?

--- src/iv/kernel/estimation.r
+++ src/iv/kernel/estimation.r
@@ -15,9 +15,14 @@ beta0hat <- function(x, Z) {
 }
 
 theta0_tsls. <- function(y, x, Z) {
-  P_Z <- Z %*% solve(t(Z) %*% Z) %*% t(Z)
-  theta0_hat <- (t(x) %*% P_Z %*% y) / (t(x) %*% P_Z %*% x) %>%
-    as.numeric
+  ZtZ <- t(Z) %*% Z
+  # P_Z <- Z %*% solve(t(Z) %*% Z) %*% t(Z)
+  # theta0_hat <- (t(x) %*% P_Z %*% y) / (t(x) %*% P_Z %*% x) %>%
+  #   as.numeric
+  
+  num <- (t(x) %*% Z %*% solve(ZtZ, y)) %>% as.numeric
+  denom <- (t(x) %*% Z %*% solve(ZtZ, x)) %>% as.numeric
+  theta0_hat <- num / denom
 
   sigma0_hhat <- sd(y - x %*% theta0_hat)
   var_theta0_hat <- sigma0_hhat^2 / (t(x) %*% P_Z %*% x)
