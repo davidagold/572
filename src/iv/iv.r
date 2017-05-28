33c1f1ccca2c086f9ae2e132c3056b13163f69e7
David Gold <david.gold.a@gmail.com>
2017-05-28 05:16:58 -0700

    Fix source file path

--- src/iv/iv.r
+++ src/iv/iv.r
@@ -1,5 +1,5 @@
 #!/usr/bin/env Rscript
 # setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-source("kernel/trial.R", chdir = TRUE)
+source("kernel/trial.r", chdir = TRUE)
 
 trial(res_dir = "/homes/dag89/572/res/iv")
