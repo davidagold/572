d06b2395a9117fee2ce9024919738c5cc42119b4
David Gold <david.gold.a@gmail.com>
2017-05-28 19:09:36 -0700

    Fix configurations

--- src/iv/config/configs.r
+++ src/iv/config/configs.r
@@ -13,17 +13,15 @@ configs. <- function() {
   args = commandArgs(trailingOnly=TRUE)
   config_dir <- args[1] %>% as.character
   
-  n <- c(100, 500)
-  pz <- c(100)
-  Fstar <- c(0, 10, 40, 160)
-  theta0 <- c(1)
+  .n <- data.frame(n = c(100, 500))
+  .pz <- data.frame(pz = c(100))
+  .Fstar <- data.frame(Fstar = c(0, 10, 40, 160))
+  .theta0 <- data.frame(theta0 = c(1))
   
-  data.frame(
-    n = n,
-    pz = pz,
-    Fstar = Fstar,
-    theta0 = theta0
-  ) %>%
+  .configs <- list(.n, .pz, .Fstar, .theta0)
+  .configs %>%
+    map(~ mutate(., id = 1)) %>%
+    reduce(inner_join, by = "id") %>%
     tibble::rownames_to_column(var = "config_id") %>%
     write.csv(paste(config_dir, "configs.csv", sep="/"))
 }
