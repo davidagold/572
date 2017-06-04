14e9b7c931ed771bf82c08bec2a3b2e221ba0f69
David Gold <david.gold.a@gmail.com>
2017-06-03 18:35:16 -0700

    Utils

--- src/utils.r
+++ src/utils.r
@@ -13,5 +13,7 @@ suppressWarnings(library(mvtnorm))
 zeros <- function(p) rep(0, p)
 ones <- function(p) rep(1, p)
 P <- function(Z, b, Zt = t(Z)) { Z %*% solve(Zt %*% Z, Zt %*% b) }
-mysd <- function(x) { sqrt( sum((x - mean(x))^2) / length(x)) }
+my.sd <- function(x) { mean((x - mean(x))^2) %>% sqrt }
 st <- function(x, lambda) { sign(x)*max(abs(x) - lambda, 0) }
+
+my.scale <- function(X) { scale(X, scale = apply(X, 2, my.sd))}
