7207640c90466d07393f39300db6e0ec4702f7fd
David Gold <david.gold.a@gmail.com>
2017-06-05 20:17:04 -0700

    Speed things up

--- src/est/lambda.jl
+++ src/est/lambda.jl
@@ -1,6 +1,7 @@
 using Lasso
 
-function σ̂_iter(y, X; γ=.05, c=1.1, ψ=.1, ϵ=.1, K=10, L=100, verbose=true)
+function σ̂_iter(y::AbstractVector{T}, X::AbstractArray{T};
+                γ=.05, c=1.1, ψ=.1, ϵ=.1, K=10, L=100, verbose=true) where T<:Real
   n, p = size(X)
   σ̂_k0 = Inf
   x̃ = map(j -> cor(y, X[:,j]), 1:p) |> A -> findmax(A)[2] |> j -> X[:,j]
@@ -8,12 +9,37 @@ function σ̂_iter(y, X; γ=.05, c=1.1, ψ=.1, ϵ=.1, K=10, L=100, verbose=true)
   if verbose; println("σ̂ (k = ", 0,") = ", σ̂_k1); end
   ν = .2 * std(y)
   k = 1
+
+  nλ = 50
+  ȳ, σ2_X, Xy, XX = Lasso.pre_lasso(y, X, false)
+  λ_max = maximum(abs, Xy)/n
+  λ_min = ϵ * λ_max
+  λs = exp.(range(log(λ_max), -(log(λ_max)-log(λ_min))/nλ, nλ))
+
+
   while (abs(σ̂_k1 - σ̂_k0) > ν) & (k <= K)
     σ̂_k0 = σ̂_k1
     λ = c * σ̂_k1 * sqrt(2*log(2*p)/n)
-    fit = lasso(y, X, λ = λ, verbose = true)
+    λid = map(x -> abs(x-λ), λs) |> A -> findmin(A)[2]
+    # λs[λid] = λ
+    splice!(λs, λid+1:λid, λ)
 
-    σ̂_k1 = mean((y .- fit.β0 - X * fit.β).^2)
+    βs = zeros(T, p, nλ)
+    for k in 2:λid
+      if verbose; println("k = ", k); end
+      λ = λs[k]
+      β = βs[:,k-1]
+      @inbounds for l in 1:L
+        for j in shuffle(1:p)
+          xjr = Xy[j] - Lasso.inner_prod(XX, j, β)
+          Gj = xjr/n + σ2_X[j]*β[j]
+          β[j] = Lasso.st(Gj, λ)/σ2_X[j]
+        end
+      end
+      βs[:,k] = β
+    end
+    β = βs[λid]
+    σ̂_k1 = mean((y .- ȳ .- X * β).^2)
     k = k + 1
     if verbose; println("σ̂ (k = ", k,") = ", σ̂_k1); end
   end
--- src/est/lasso.jl
+++ src/est/lasso.jl
@@ -32,12 +32,53 @@ struct LassoFit{T}
   standardized::Bool
 end
 
-# function lasso(y::AbstractVector{T}, X::AbstractMatrix{T};
-#                λ::Float64 = Inf, ϵ::Float64 = -1., nλ::Int = 100,
-#                λs::Vector{Float64} = Vector{Float64}(), H::Int = 100,
-#                standardize::Bool = false, verbose::Bool = false) where T<:Real
+function pre_lasso(y::AbstractVector{T}, X::AbstractArray{T}, standardize) where T<:Real
+  n, p = size(X)
+  ȳ = mean(y)
+  y = y - ȳ
+  μ_X = map(j -> mean(X[:,j]), 1:p)
+  σ2_X = Vector{T}(p)
+  for j in 1:p
+    σ2_X[j] = mean(X[:,j].^2) - μ_X[j]^2
+  end
+  σ_X = sqrt.(σ2_X)
+  if standardize
+    X = X ./ σ_X'
+    σ2_X = ones(p)
+  end
+
+  Xy = X' * y; XX = Matrix(X' * X)
+  MM = Array{T}(p, p)
+  μ_Xj_by_σ_Xj = μ_X ./ σ_X
+  for j in 1:p
+    t = μ_Xj_by_σ_Xj[j]
+    for k in j:p
+      MM[j,k] = n * t * μ_Xj_by_σ_Xj[k]
+    end
+  end
+  MM = Symmetric(MM)
+  # M = ones(T, n, p) .* (μ_X ./ σ_X)'
+  XM = Array{T}(p, p)
+  for j in 1:p
+    Xjsum = sum(X[:,j])
+    for k in 1:p
+      XM[j,k] = Xjsum * μ_Xj_by_σ_Xj[k]
+    end
+  end
+  My = Vector{T}(p)
+  for j in 1:p
+    My[j] = sum(y) * μ_Xj_by_σ_Xj[j]
+  end
+  # XM = X' * M
+  # My = M' * y
+  Ay = Xy - My
+  AA = XX - XM - XM' + MM
+  return ȳ, σ2_X, Ay, AA
+end
+
+# function pre_lasso(y::AbstractVector{T}, X::AbstractMatrix{T}, standardize) where T<:Real
 #   n, p = size(X)
-#   ȳ = mean(y); β0 = ȳ
+#   ȳ = mean(y)
 #   y = y - ȳ
 #   μ_X = map(j -> mean(X[:,j]), 1:p)
 #   σ2_X = map(j -> std(X[:,j], mean = μ_X[j], corrected=false), 1:p)
@@ -48,119 +89,8 @@ end
 #   else
 #     X = X .- μ_X'
 #   end
-#
-#   ϵ = ifelse(ϵ < 0, ifelse(n > p, .0001, .01), ϵ)
-#   if isempty(λs)
-#     λ_max = map(j -> sum(X[:,j] .* y), 1:p) |>
-#         _ -> maximum(abs, _)/n
-#     λ_min = ϵ * λ_max
-#     λs = exp.(range(log(λ_max), -(log(λ_max)-log(λ_min))/nλ, nλ))
-#   end
-#   if λ < Inf
-#     λid = map(x -> abs(x-λ), λs) |> A -> findmin(A)[2]
-#     λs[λid] = λ
-#   else
-#     λ = λ_max
-#     λid = 1.
-#   end
-#
 #   Xy = X' * y; XX = X' * X
-#   βs = zeros(T, p, nλ)
-#   for k in 2:nλ
-#     if verbose; println("k = ", k); end
-#     λ = λs[k]
-#     β = βs[:,k-1]
-#     for l in 1:L
-#       for j in shuffle(1:p)
-#         xjr = Xy[j] - inner_prod(XX, j, β)
-#         Gj = xjr/n + σ2_X[j]*β[j]
-#         β[j] = st(Gj, λ)/σ2_X[j]
-#       end
-#     end
-#     βs[:,k] = β
-#   end
-#   βs
-#   return LassoFit(λs, λ, βs, βs[:,λid], β0, λid)
-# end
-
-function pre_lasso(y::AbstractVector{T}, X::AbstractSparseMatrix{T}, standardize) where T<:Real
-  n, p = size(X)
-  ȳ = mean(y)
-  y = y - ȳ
-  μ_X = map(j -> mean(X[:,j]), 1:p)
-  σ2_X = map(j -> std(X[:,j], mean = μ_X[j], corrected=false), 1:p)
-  σ_X = sqrt.(σ2_X)
-  if standardize
-    X = X ./ σ_X'
-    σ2_X = ones(p)
-  end
-
-  Xy = X' * y; XX = X' * X
-  M = ones(T, n, p) .* (μ_X ./ σ_X)'
-  MM = M' * M
-  XM = X' * M
-  My = M' * y
-  Ay = Xy - My
-  AA = XX - XM - XM' + MM
-  return ȳ, σ2_X, Ay, AA
-end
-
-function pre_lasso(y::AbstractVector{T}, X::AbstractMatrix{T}, standardize) where T<:Real
-  n, p = size(X)
-  ȳ = mean(y)
-  y = y - ȳ
-  μ_X = map(j -> mean(X[:,j]), 1:p)
-  σ2_X = map(j -> std(X[:,j], mean = μ_X[j], corrected=false), 1:p)
-  σ_X = sqrt.(σ2_X)
-  if standardize
-    X = (X .- μ_X')./σ_X'
-    σ2_X = ones(T, p)
-  else
-    X = X .- μ_X'
-  end
-  Xy = X' * y; XX = X' * X
-  return ȳ, σ2_X, Xy, XX
-end
-#
-# function lasso(y::AbstractVector{T}, X::AbstractMatrix{T};
-#                λ::T = zero(T), ϵ::Float64 = -1., nλ::Int = 100,
-#                λs::Vector{T} = Vector{T}(), L::Int = 100,
-#                standardize::Bool = false, verbose::Bool = false) where T<:Real
-#
-#   n, p = size(X)
-#   ȳ, σ2_X, Xy, XX = pre_lasso(y, X, standardize)
-#   ϵ = ifelse(ϵ < 0, ifelse(n > p, .0001, .01), ϵ)
-#   if isempty(λs)
-#     λ_max = maximum(abs, Xy)/n
-#     λ_min = ϵ * λ_max
-#     λs = exp.(range(log(λ_max), -(log(λ_max)-log(λ_min))/nλ, nλ))
-#   else
-#     nλ = length(λs)
-#   end
-#   if λ > 0
-#     λid = map(x -> abs(x-λ), λs) |> A -> findmin(A)[2]
-#     λs[λid] = λ
-#   else
-#     λ = maximum(λs)
-#     λid = 1
-#   end
-#
-#   βs = zeros(T, p, nλ)
-#   for k in 2:nλ
-#     if verbose; println("k = ", k); end
-#     λ = λs[k]
-#     β = βs[:,k-1]
-#     for l in 1:L
-#       for j in shuffle(1:p)
-#         xjr = Xy[j] - inner_prod(XX, j, β)
-#         Gj = xjr/n + σ2_X[j]*β[j]
-#         β[j] = st(Gj, λ)/σ2_X[j]
-#       end
-#     end
-#     βs[:,k] = β
-#   end
-#   βs
-#   return LassoFit(λs, λs[λid], ones(T, p), βs, βs[:,λid], ȳ, λid, standardize)
+#   return ȳ, σ2_X, Xy, XX
 # end
 
 function lasso(y::AbstractVector{T}, X::AbstractMatrix{T};
